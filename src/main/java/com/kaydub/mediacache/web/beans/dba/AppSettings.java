3e20e497739e9a0456ebff8a7f3c7aa6a37f1c18
Amy Detwiler <detwiler.amy@gmail.com>
2015-04-12 21:52:16 -0400

    not sure if this is right, but I figured we'd talk about it tomorrow.

---
+++ src/main/java/com/kaydub/mediacache/web/beans/dba/AppSettings.java
@@ -0,0 +1,18 @@
+package com.kaydub.mediacache.web.beans.dba;
+
+/**
+ *
+ * @author AMY
+ */
+public class AppSettings {
+    private String appSettings;
+    
+    public String getAppSettings(){
+        return appSettings;        
+    }
+    
+    public void setAppSettings(String appSettings){
+        this.appSettings = appSettings;        
+    }
+    
+}
