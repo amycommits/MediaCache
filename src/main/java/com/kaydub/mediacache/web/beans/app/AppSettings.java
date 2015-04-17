cf6b3de32d58db7551853bacf5224e77a4702209
Amy Detwiler <detwiler.amy@gmail.com>
2015-04-16 21:53:50 -0400

    Latest from webapp for 4/16.

---
+++ src/main/java/com/kaydub/mediacache/web/beans/app/AppSettings.java
@@ -0,0 +1,58 @@
+package com.kaydub.mediacache.web.beans.app;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author AMY
+ */
+public class AppSettings implements Serializable {
+    private String key;
+    private String value;
+    private long id;
+    
+  
+
+    /**
+     * @return the key
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * @param key the key to set
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+}
